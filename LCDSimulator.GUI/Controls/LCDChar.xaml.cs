0d69e7ca4ec38dbc01e345fa79ae8432e4ed2683
TollyH <ptolemyhill@sky.com>
2024-04-21 23:59:20 +0100

    Create initial window and LCD character

---
+++ LCDSimulator.GUI/Controls/LCDChar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LCDSimulator.GUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for LCDChar.xaml
+    /// </summary>
+    public partial class LCDChar : UserControl
+    {
+        public LCDChar()
+        {
+            InitializeComponent();
+        }
+    }
+}
