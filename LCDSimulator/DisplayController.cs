513b3ace588e59b384827c2de4f86ac0f2f38f9c
TollyH <ptolemyhill@sky.com>
2024-04-23 01:06:30 +0100

    Define initial display controller properties

---
+++ LCDSimulator/DisplayController.cs
@@ -0,0 +1,20 @@
+﻿namespace LCDSimulator
+{
+    public class DisplayController
+    {
+        // MPU Pins
+        public bool RegisterSelect { get; set; }
+        public bool ReadWrite { get; set; }
+        public byte DataBus { get; set; }
+
+        // Internal State (Inaccessible on a real controller without going through MPU)
+        public byte AddressCounter { get; private set; }
+        public bool BusyFlag { get; private set; }
+        public byte InstructionRegister { get; private set; }
+        public byte DataRegister { get; private set; }
+
+        public byte[] DisplayDataRAM { get; } = new byte[80];
+        public byte[] CharacterGeneratorRAM { get; } = new byte[64];
+        public byte[] CharacterGeneratorROM { get; } = new byte[4096];
+    }
+}
